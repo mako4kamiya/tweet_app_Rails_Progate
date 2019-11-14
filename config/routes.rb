567a9146b1fa0519b97a03044ab409354697db02
Mako Kamiya <mako4kamiya@gmail.com>
2019-11-14 18:58:24 +0900

    Ruby on Rails 道場コースⅠの終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'posts/index' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
+
+  get '/' => 'home#top'
+  get '/about' => 'home#about'
 end
