aee4bcbdcc846ad905da9f6af6cc228c907d4e05
Mako Kamiya <mako4kamiya@gmail.com>
2019-11-16 00:01:32 +0900

    投稿の編集機能を実装しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/:id/edit' => 'posts#edit'
   get 'posts/new' => 'posts#new'
   get 'posts/index' => 'posts#index'
   get 'posts/:id' => 'posts#show'
   post 'posts/create' => 'posts#create'
+  post 'posts/:id/update' => 'posts#update'
 
   get '/' => 'home#top'
   get '/about' => 'home#about'
