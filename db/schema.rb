0e97784353351ff3fd420b3551bea2eb35904a96
Mako Kamiya <mako4kamiya@gmail.com>
2019-11-20 17:15:32 +0900

    パスワードを暗号化しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_044822) do
+ActiveRecord::Schema.define(version: 2019_11_20_074337) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_044822) do
     t.string "image_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
